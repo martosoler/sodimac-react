59fec0e5ade39389387f3ed16503dd3e837d1031
Leandro Soler <lean@devgurus.io>
2018-01-11 10:46:31 -0300

    Using uri instead of host

--- src/services/quotes.js
+++ src/services/quotes.js
@@ -1,21 +1,12 @@
 import axios from 'axios';
 
 // const apiPrefix = 'https://sodimac-quotes-api.azurewebsites.net'
-const apiPrefix = 'https://solerthekong.cloudapp.net:8443'
+const kongHost = 'https://solerthekong.cloudapp.net:8443'
 const code = 'KMuoX5FjRO9m/ViWxjC3eDAGR6EijHLtGswioGnQalFQBV38mhyyWQ=='
-
-axios.interceptors.request.use((config) => {
-    // Do something before request is sent
-    config.headers.Host = 'sodimac-quotes-api.azurewebsites.net';
-    return config;
-  }, (error) => {
-    console.error(error);
-    return Promise.reject(error);
-  });
 
 const getQuotes = () => {
   console.log('getting quotes');
-  return axios.get(`${apiPrefix}/api/list?code=${code}`)
+  return axios.get(`${kongHost}/quotes/api/list?code=${code}`)
     .then(res => res.data)
     .catch((error) => {
       console.error('Error getting quotes', error);
